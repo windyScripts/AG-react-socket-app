6672b68c7638c0be4c3d4a263baacdde37aa39b7
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-09-14 23:35:01 +0530

    Adds basic socket functionality to backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -21,10 +23,31 @@ app.use(bodyParser.json());
 
 // Add routes here
 
-async function start() {
-    await mongoose.connect(process.env.DB_STRING);
-    console.log('Database connected. :)');
-    app.listen(process.env.PORT || 3000);
-  }
+const httpServer = createServer(app);
+
+const start = async () => {
+  await mongoose.connect(process.env.DB_STRING);
+  console.log('Database connected. :)');
+  httpServer.listen(process.env.PORT || 3000);
+};
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    allowedHeaders: ['Authorization'],
+  },
+});
+
+io.on('connection', socket => {
+  const token = socket.handshake.headers.authorization;
+  socket.id = getSocketId(token);
+  socket.on('join-room', (room, cb) => {
+    socket.join(room);
+    cb(`Connected to ${room}`);
+  });
+
+  // Socket commands
   
-  start();
+});
+
+start();
