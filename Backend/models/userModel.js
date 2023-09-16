ced22446faa397ecd140998c7ef1077a9f17cefd
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-09-16 18:27:33 +0530

    updates user model

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
+  username: {
     type: String,
     required: true,
   },
@@ -15,9 +15,9 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  currentlyPlaying:{
-    type: Boolean,
-    default: true
+  currentSocketId:{
+    type: String,
+    default: '',
   }
 });
 
